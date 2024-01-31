5b924c97a984f01ec599ae0f11df3242361f1bad
jeydeel <157578262+jeydeel@users.noreply.github.com>
2024-01-31 14:08:09 +0800

    Add files via upload

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	string name;
-	cout<<"Welcome to C++ language Programming";
-	cout<<"\nGood morning pal!, what is your name? ";
-	cin>>name;
-	cout<<"Hi "+ name + ", Welcome to C++ Programming Class.\n";
+int main (){
+  int markA, markB, markC, totalSum;
+  float averageMark;
+  cout<<"Please enter the mark of the first student: " ;
+  cin>>markA;
+  cout<<"Please enter the mark of the second student: ";
+  cin>>markB;
+  cout<<"Please enter the mark of the last student: ";
+  cin>>markC;
+  totalSum = markA + markB + markC;
+  averageMark = (float)totalSum / 3;
+  cout<<"The average mark of the three students is "<<averageMark;
+  
 }
